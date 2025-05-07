6972c841194f50c213e7c71c45bcce1e628b1758
Jose David <josedavidalviares@gmail.com>
2025-05-07 09:13:05 +0200

    init: commit base con el proyecto de Android Studio
    Base del proyecto, Empty project y esperanza ;)

---
+++ app/src/main/java/com/icr/proyecto_abp_bloc_de_notas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.icr.proyecto_abp_bloc_de_notas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.icr.proyecto_abp_bloc_de_notas.ui.theme.Proyecto_ABP_Bloc_de_NotasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Proyecto_ABP_Bloc_de_NotasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Proyecto_ABP_Bloc_de_NotasTheme {
+        Greeting("Android")
+    }
+}
